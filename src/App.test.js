1ab2f932e8f587a2c353d500194c646d4ee9f4ee
luckaruto <lucmten@gmail.com>
2024-08-08 19:00:22 +0700

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("Privacy Policy", () => {
-  render(<App />);
-  const header = screen.getByText(/Privacy Policy/i);
-  expect(header).toBeInTheDocument();
-});
